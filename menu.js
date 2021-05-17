9957816b70e419d92de5ae231a30b24b949c21fc
guilherme <78052265+Guilherme275149@users.noreply.github.com>
2021-05-17 15:21:08 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,385 @@
+const help = (p, date, user, wame) => {
+return `╔╦══• •✠• 𝐂𝐎𝐌𝐀𝐍𝐃𝐎𝐒 •✠ • •══╦╗
+║
+║ 𝐁𝐎𝐓 : 「 𝐁𝐄𝐋𝐋𝐀 」
+║
+║ 𝐏𝐑𝐄𝐅𝐈𝐗𝐎 : /
+║
+║ 𝐃𝐎𝐍𝐎 : wa.me/+5575998868032
+║
+║𝘿𝘼𝙏𝘼 𝙀 𝙃𝙊𝙍𝘼 : ${date} 
+║
+║𝙐𝙎𝙐𝘼𝙍𝙄𝙊 : ${user} 
+║
+║𝙒𝘼𝙈𝙀 : ${wame} 
+║𝐄𝐔 𝐅𝐔𝐈 𝐀𝐓𝐔𝐀𝐋𝐈𝐙𝐀𝐃𝐀 
+║𝐃𝐄 𝐄𝐋𝐈𝐙𝐀𝐁𝐄𝐓𝐇 𝐏𝐀𝐑𝐀 𝐁𝐄𝐋𝐋𝐀
+║𝐍𝐀𝐎 𝐅𝐋𝐎𝐃𝐄 𝐌𝐄𝐔 𝐂𝐇𝐀𝐓 𝐀𝐆𝐑𝐀𝐃𝐄𝐂𝐎
+╚╩══• •✠• 𝐂𝐎𝐌𝐀𝐍𝐃𝐎𝐒 •✠ • •══╩╝
+
+
+┏   「⚡𝙿𝚛𝚒𝚗𝚌𝚒𝚙𝚊𝚒𝚜⚡」
+╟➤listblockcmd 
+╟➤listpalavra 
+╟➤info 
+╟➤comandos
+╟➤ping 
+╟➤destrava 
+╟➤sugerircmd  [Texto]
+╟➤botvip 
+╟➤blocklist 
+╟➤blacklist 
+╟➤criador 
+╟➤toimg  ❮ᶜᵃᵖᵗⁱᵒⁿ, ʳᵉᵖˡʸ ⁱᵐᵍ❯
+╽┷╼❥
+
+
+┏   「 ✨𝚂𝚝𝚒𝚌𝚔𝚎𝚛𝚜✨ 」
+╟➤stiker  ❮ᶜᵃᵖᵗⁱᵒⁿ, ʳᵉᵖˡʸ ⁱᵐᵍ❯
+╟➤sticker  ❮ᶜᵃᵖᵗⁱᵒⁿ, ʳᵉᵖˡʸ ⁱᵐᵍ❯
+╟➤fstiker  ❮ᶜᵃᵖᵗⁱᵒⁿ, ʳᵉᵖˡʸ ⁱᵐᵍ❯
+╟➤fsticker  ❮ᶜᵃᵖᵗⁱᵒⁿ, ʳᵉᵖˡʸ ⁱᵐᵍ❯
+╟➤nstiker  ❮ᵖᵃᶜᵏᵃᵍᵉ❯ ❮ᵃᵘᵗʰᵒʳ❯
+╟➤nsticker  ❮ᵖᵃᶜᵏᵃᵍᵉ❯ ❮ᵃᵘᵗʰᵒʳ❯
+╟➤nfstiker  ❮ᵖᵃᶜᵏᵃᵍᵉ❯ ❮ᵃᵘᵗʰᵒʳ❯
+╟➤nfsticker  ❮ᵖᵃᶜᵏᵃᵍᵉ❯ ❮ᵃᵘᵗʰᵒʳ❯
+╟➤nstickerv2  ❮ᵖᵃᶜᵏᵃᵍᵉ❯ ❮ᵃᵘᵗʰᵒʳ❯
+╟➤attp  [Texto]
+╟➤macaco 
+╽┷╼❥
+
+
+┏   「 🎥𝚅𝚒́𝚍𝚎𝚘🎥 」
+╟➤bold  [Texto]
+╟➤poly  [Texto]
+╟➤glowing  [Texto]
+╟➤colorful  [Texto]
+╟➤army  [Texto]
+╟➤retro  [Texto]
+╟➤frame  [Seg] [Marque O Video]
+╟➤mute  [Marque O Video]
+╟➤rapidovid  [Marque o Video]
+╟➤lentovid  [Marque O Video]
+╟➤togif  [Marque o Video]
+╟➤resizevid  ❮ˡᵃᵍ❯ ❮ᵃˡᵗ❯ ❮ʳᵉᵖˡʸ ᵛⁱᵈ❯
+╽┷╼❥
+
+
+┏   「 🎨𝙻𝚘𝚐𝚘𝚜🎨 」
+╟➤missionpassed  ❮ᵗ1❯ | ❮ᵗ2❯
+╟➤nulis  [Texto]
+╟➤watercolor  [Texto]
+╟➤neonlogo  [Texto]
+╟➤flowerlogo  [Texto]
+╟➤matrixlogo  [Texto]
+╟➤thunderlogo  [Texto]
+╟➤silverplaca  [Texto]
+╟➤goldplaca  [Texto]
+╟➤narutologo  [Texto]
+╟➤phlogo  ❮ᵗ1❯ | ❮ᵗ2❯
+╟➤mineconquista  ❮ᵗ1❯ | ❮ᵗ2❯
+╟➤shadow  [Texto]
+╟➤glitch  ❮ᵗ1❯ | ❮ᵗ2❯
+╟➤ravetxt  ❮ᵗ1❯ | ❮ᵗ2❯
+╟➤woodtxt  ❮ᵗ1❯ | ❮ᵗ2❯
+╟➤neon  ❮ᵗ1❯ | ❮ᵗ2❯
+╟➤sunset  ❮ᵗ1❯ | ❮ᵗ2❯
+╟➤gimage  [Texto]
+╟➤pglass  [Texto]
+╟➤neonligth  [Texto]
+╟➤coffe  [Texto]
+╟➤galaxywp  [Texto]
+╟➤whatis  [Texto]
+╟➤txt3d  [Texto]
+╽┷╼❥
+
+
+┏   「 🔍𝙿𝚎𝚜𝚚𝚞𝚒𝚜𝚊🔎 」
+╟➤fortniteshop 
+╟➤imagesearch [Marque A Imagem]
+╟➤map [Nome]
+╟➤brainlysrc [Nome]
+╟➤search [Nome]
+╟➤ssweb [Url]
+╟➤mediafire [Url]
+╟➤mob 
+╟➤minecraft 
+╟➤covidbrasil 
+╟➤covidmundo 
+╟➤foxnews 
+╟➤lofi 
+╟➤boy 
+╟➤girl 
+╟➤egirl 
+╟➤eboy 
+╟➤pinterest [Nome]
+╟➤wallpaper 
+╟➤fuckmylife 
+╟➤waifu 
+╟➤belle 
+╟➤saycat 
+╟➤biblia 
+╟➤animesrc [Nome Do Anime]
+╟➤mangasrc [Nome Do Anime]
+╟➤playstore [Nome Do App]
+╟➤happymod [Nome Do App]
+╟➤pokemon 
+╟➤ip  ❮ip❯
+╟➤signome  [Nome]
+╟➤gimage  [Nome]
+╽┷╼❥
+
+
+┏   「 ⏳𝙲𝚘𝚗𝚝𝚊𝚐𝚎𝚖⏳ 」
+╟➤halloween  [Ano]
+╟➤kidday  [Ano]
+╟➤motherday  [Ano]
+╟➤fatherday  [Ano]
+╟➤carnaval  [Ano]
+╟➤republic  [Ano]
+╟➤goodfriday  [Ano]
+╟➤finados  [Ano]
+╟➤ourlady  [Ano]
+╟➤independence  [Ano]
+╟➤workday  [Ano]
+╟➤tiradentes  [Ano]
+╟➤pascoa  [Ano]
+╟➤natal  [Ano]
+╟➤anonovo 
+╽┷╼❥
+
+
+┏   「 😈 𝙿𝚘𝚛𝚗𝚘😈 」
+╟➤packmega18 
+╟➤nhentai  ❮code❯
+╟➤randomhentai 
+╟➤gangbang 
+╟➤nsfwblowjob 
+╟➤panties 
+╟➤wallhentai 
+╟➤tentacles 
+╟➤nsfwneko 
+╟➤nsfwtrap 
+╟➤nsfw   [1 Para Ativa /0 Para Desativar]
+╟➤yaoi 
+╟➤ahegao 
+╟➤ecchi 
+╟➤cum 
+╟➤anal 
+╟➤pussy 
+╟➤ero 
+╟➤ahegao 
+╟➤yuri 
+╟➤hentaigif 
+╟➤hentainekogif 
+╟➤futanari 
+╟➤nsfwfeet 
+╟➤nsfwfeetgif 
+╟➤girlmasturbate 
+╟➤girlmasturbategif 
+╟➤lesbian 
+╟➤eroyuri 
+╟➤eroneko 
+╟➤erokitsune 
+╟➤spank 
+╽┷╼❥
+
+
+┏   「 🗣️𝚟𝚘𝚣 𝚖𝚘𝚍🗣️ 」
+╟➤esquilo  [Marque O Audio]
+╟➤grave  [Marque O Audio]
+╟➤estourar  [Marque O Audio]
+╟➤rapidoaudio  [Marque O Audio]
+╟➤lentoaudio  [Marque O Audio]
+╟➤bass [Marque O Audio]
+╽┷╼❥
+
+
+┏   「 🎵 𝙰𝚞𝚍𝚒𝚘🎵 」
+╟➤shazam  [Marque O Audio]
+╟➤gtts  [Idioma] [Texto]
+╟➤playlist 
+╟➤lyrics 
+╟➤ptlyrics 
+╟➤play  [Nome Da Musica]
+╟➤playv2  [Nome Da Musica]
+╟➤tomp3  ❮ᶜᵃᵖᵗⁱᵒⁿ, ʳᵉᵖˡʸ ᵛⁱᵈᵉᵒ❯
+╟➤ytsearch  [Nome]
+╟➤ytmp4  [Yt Url]
+╟➤ytmp3  [Yt Url]
+╽┷╼❥
+
+
+┏   「 🧾𝚃𝚎𝚡𝚝𝚘🧾 」
+╟➤frase 
+╟➤wppim 
+╟➤db 
+╽┷╼❥
+
+
+┏   「 🇯🇵𝙰𝚗𝚒𝚖𝚎𝚜🇯🇵 」
+╟➤randomanime 
+╟➤randomshota 
+╟➤randomkiss 
+╟➤randomcry 
+╟➤randomhug 
+╟➤nekoanime 
+╟➤wait  ❮ᶜᵃᵖᵗⁱᵒⁿ, ʳᵉᵖˡʸ ᵛⁱᵈᵉᵒ❯
+╟➤slap 
+╟➤smile 
+╟➤piscada 
+╟➤dance 
+╟➤cringe 
+╟➤blush 
+╟➤gore 
+╟➤acenar 
+╟➤lick 
+╟➤bully 
+╟➤kemonomimi 
+╽┷╼❥
+
+
+┏   「 👾𝙲𝚘𝚍𝚒𝚐𝚘𝚜👾 」
+╟➤txtomorse  [Texto]
+╟➤morsetotxt  [Texto]
+╟➤ebinary  [Texto]
+╟➤dbinary  [Texto]
+╽┷╼❥
+
+
+┏   「🏆𝚛𝚊𝚗𝚔𝚒𝚗𝚐𝚜🏆」
+╟➤ranklindo 
+╟➤rankfeio 
+╟➤rankgado 
+╟➤rankcomu 
+╟➤ranknazi 
+╟➤rankqi 
+╟➤rankfofo 
+╟➤rankotaku 
+╟➤rankgay 
+╽┷╼❥
+
+
+┏   「🔮𝙿𝚛𝚎𝚟𝚒𝚜𝚘̃𝚎𝚜🔮」
+╟➤cassino 
+╟➤ship  [Marque O Alvo 1] [Marque O Alvo 2]
+╟➤corno  [Marque O Alvo]
+╟➤nazista  [Marque O Alvo]
+╟➤comunista  [Marque O Alvo]
+╟➤dado 
+╟➤gay  [Marque O Alvo]
+╟➤qi  [Marque O Alvo]
+╟➤seudia  [Marque O Alvo]
+╟➤feio  [Marque O Alvo]
+╟➤bv  [Marque O Alvo]
+╟➤gado  [Marque O Alvo]
+╟➤gostoso  [Marque O Alvo]
+╟➤gostosa  [Marque O Alvo]
+╟➤randomship  ❮ⁿᵘᵐ1 ᵒʳ n/a❯
+╽┷╼❥
+
+
+┏   「 ❌𝙰𝚗𝚝𝚒𝚜❌ 」
+╟➤antilink   [1 Para Ativa /0 Para Desativar]
+╟➤antiporn   [1 Para Ativa /0 Para Desativar]
+╟➤antifake   [1 Para Ativa /0 Para Desativar]
+╟➤antilinkhard   [1 Para Ativa /0 Para Desativar]
+╽┷╼❥
+
+
+┏   「 👥𝙶𝚛𝚞𝚙𝚘𝚜👥 」
+╟➤fechargp 
+╟➤mudardesc 
+╟➤mudarnome 
+╟➤abrirgp 
+╟➤linkgroup 
+╟➤listadmin 
+╟➤listonline 
+╟➤leave 
+╟➤kick  [@alvo]
+╟➤ban  [marque uma mensagem pra remove]
+╟➤promote  [@alvo
+╟➤demote  [@alvo]
+╟➤add  [EX 5575986xxxx]
+╟➤welcome  [1/0] [Boas Vindas]
+╟➤simih  [1 Para Ativa/0 Para Desativa]
+╟➤autostickerimg  [1 Para Ativa / 0 Para Desativa]
+╟➤autoreply   [1 Para Ativa /0 Para Desativar]
+╟➤marcar 
+╟➤marcar2 
+╟➤marcar3 
+╟➤hidemarcar  [Texto]
+╽┷╼❥
+
+
+┏   「 🕵️𝚂𝚝𝚊𝚕𝚔𝚎𝚛🕵️ 」
+╟➤instastalk  [Nome]
+╟➤githubstalk  [Nome]
+╟➤tiktokstalk  [Nome]
+╟➤twitterstalk  [ Nome ]
+╟➤ffstalk  [ID Do Jogador]
+╟➤ytstalk  [Nome]
+╽┷╼❥
+
+
+┏   「 😳𝙸𝚗𝚝𝚎𝚛𝚊𝚌̧𝚊̃𝚘😳 」
+╟➤vsf 
+╟➤botfofo 
+╟➤pedro 
+╟➤botgostoso 
+╟➤botviado 
+╟➤botbaianor 
+╟➤botfdp 
+╟➤botfeio 
+╟➤botputa 
+╟➤botgay 
+╟➤botcorno 
+╟➤vtmnc 
+╟➤bomdia 
+╟➤boatarde 
+╟➤boanoite 
+╟➤lindo 
+╟➤cheguei 
+╽┷╼❥
+
+
+┏   「 😎𝙲𝚛𝚒𝚊𝚍𝚘𝚛😎 」
+╟➤blockcmd  ❮ᶜᵐᵈ ˡᵉˢˢ ᵖʳᵉᶠⁱˣ❯
+╟➤unblockcmd  ❮ᶜᵐᵈ ˡᵉˢˢ ᵖʳᵉᶠⁱˣ❯
+╟➤alerta  [Texto]
+╟➤infinityblock   [1 Para Ativa /0 Para Desativar]
+╟➤antiligar   [1 Para Ativa /0 Para Desativar]
+╟➤addpalavra  [Texto]
+╟➤removepalavra  [Texto]
+╟➤limparchat  [Texto]
+╟➤ataque  [Texto]
+╟➤divulgar  [Texto]
+╟➤entrargp  [Texto]
+╟➤block  [@Marque O Número
+╟➤unblock [@Marque o Numero]
+╽┷╼❥
+
+
+┏   「 😝𝙳𝚒𝚟𝚎𝚛𝚜𝚊̃𝚘😝 」
+╟➤meme 
+╟➤memeindo 
+╟➤darkjokes 
+╽┷╼❥
+
+
+┏   「 💱𝙼𝚘𝚎𝚍𝚊💱 」
+╟➤dolarhoje 
+╟➤rublohoje 
+╟➤ienehoje 
+╟➤librahoje 
+╟➤bitcoinhoje 
+╟➤realhoje 
+╟➤cvoin  ❮ᶜ1❯|❮ᶜ2❯
+╟➤ccoin  ❮ᶜ1❯|❮ᶜ2❯|❮ⁿᵘᵐ❯
+╽┷╼❥`
+}
+
+exports.help = help
+
+
+
