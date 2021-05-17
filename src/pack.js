d72d5659cd5b4fdc8314cb48f6bb80c53764ecb9
guilherme <78052265+Guilherme275149@users.noreply.github.com>
2021-05-16 22:19:46 -0300

    Add files via upload

---
+++ src/pack.js
@@ -0,0 +1,31 @@
+const pack = (date, user, wame) => {
+return `╔╦══• •✠• PACKS •✠ • •══╦╗
+║
+║ 𝐁𝐎𝐓 : 「 𝐁𝐄𝐋𝐋𝐀 」
+║
+║ 𝐏𝐑𝐄𝐅𝐈𝐗𝐎 : /
+║
+║ 𝐃𝐎𝐍𝐎 : wa.me/+5575998868032
+║
+║𝘿𝘼𝙏𝘼 𝙀 𝙃𝙊𝙍𝘼 : ${date} 
+║
+║𝙐𝙎𝙐𝘼𝙍𝙄𝙊 : ${user} 
+║
+║𝙒𝘼𝙈𝙀 : ${wame} 
+║
+╚╩══• •✠• PACKS •✠ • •══╩╝
+Velho Se E Muito Cringe kkkkkk
+
+Belle delphine : https://photos.app.goo.gl/Jr4Qk1dFSJepPdRc7 
+
+Pack1: https://photos.app.goo.gl/phyRpNFLcAtsknfJ7 
+
+Kitty kum: https://photos.app.goo.gl/gNxLbJHGVzeWY9iS9
+ 
+Misaki Your Waifu: https://photos.app.goo.gl/M9nyHdonAJu5GRjZ9 
+
+pack: https://photos.app.goo.gl/SrogbCbnanL2PJQn7 
+
+Love Lilah : https://photos.app.goo.gl/TBbkjGGdAVHjPFt5A`
+}
+exports.pack = pack
---
+++ src/wppimune.js
@@ -0,0 +1,33 @@
+const databases = (prefix) => {
+
+return `┏━━•°•❈💥𝚍𝚊𝚝𝚊𝚋𝚊𝚜𝚎 𝚍𝚎 𝚝𝚛𝚊𝚟𝚊💥•°•━┓
+║
+║ 𝑫𝑨𝑻𝑨𝑩𝑨𝑺𝑬 𝑬𝑽𝑰𝑳 𝑽4➪9.68𝐌𝐁 TA ATUALIZADA
+║ https://www.mediafire.com/file/5p1z6dvv434jfha/DATABASE_EVIL_V4.7z/file
+║
+┗━━━━━━━━━━━━━━━━━━━━━━━━━┛`
+}
+
+exports.databases = databases
+
+const imune = (prefix) => {
+return `┏━━•❅•°•❈🌀𝚠𝚙𝚙 𝚒𝚖𝚞𝚗𝚎𝚜🌀•°•❅•━━┓
+║ 𝑯𝑰𝑫𝑹𝑨 2.0.2.1➪24.38𝑴𝑩
+║ https://www.mediafire.com/file/zfngz3anodu3j32/HYDRA_2.0.2.1.apk/file
+║
+║ 𝑷𝑬𝑹𝑻𝑼𝑩𝑨𝑻𝑰𝑶𝑵 𝑭𝑰𝑵𝑨𝑳 𝑽𝑬𝑹𝑺𝑰𝑶𝑵 1➪25.32𝑴𝑩
+║ https://www.mediafire.com/file/v51l31nawrkl1r3/perturbation_final_version1.apk/file
+║
+║ 𝑫𝑰𝑵 𝑽10 𝑷𝑹𝑰𝑽𝑨𝑻𝑬➪30.51𝑴𝑩
+║ https://www.mediafire.com/file/7hjz0k7t42mnufi/%25E2%258F%25A4%25CD%259F%25CD%259F%25CD%259E%25CD%259E%25EA%25AA%25B6%25F0%259D%2590%2583%25F0%259D%2591%25B0%25F0%259D%259A%25B4_%25F0%259D%2591%25BD%25F0%259D%259F%258F%25F0%259D%259F%258E_%25F0%259D%2591%25B7%25F0%259D%2591%25B9%25F0%259D%2591%25B0%25F0%259D%2591%25BD%25F0%259D%2591%25A8%25F0%259D%2591%25BB%25F0%259D%259A%25B5%25EA%25AB%2582.apk/file
+║
+║ 𝑯𝑰𝑷𝑵𝑶𝑻𝑰𝑪  𝑻𝑼𝑵𝑵𝑬𝑫➪23.49𝑴𝑩
+║ https://www.mediafire.com/file/h6pcn99pgdhalp0/HIPNOTIC_TUNNED__by_VITINN_OFC_.apk/file
+║
+║ 𝑩𝑹𝑶𝑲𝑳𝒀𝑵9 𝑴𝑶𝑫𝑶 𝑮𝑼𝑬𝑹𝑹𝑨➪23.94𝑴𝑩
+║ https://www.mediafire.com/file/kxyulyfgew7683l/BR0KLYN9+MODO+GUERRA+BY+BR0KLYN9.apk/file
+║
+┗━━━━━━━━━━━━━━━━━━━━━━━━━┛`
+}
+
+exports.imune = imune
